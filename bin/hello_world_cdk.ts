8285564602aa8fe321256986ae7614d0718fc524
Micha <micha@mivex.net>
2022-01-19 16:00:43 +0100

    Getting Started goes wrong

--- bin/hello_world_cdk.ts
+++ bin/hello_world_cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { HelloWorldCdkStack } from '../lib/hello_world_cdk-stack';
+import { HelloWorldCdkLambdaStack } from '../lib/hello_world_cdk-stack';
 
 const app = new cdk.App();
-new HelloWorldCdkStack(app, 'HelloWorldCdkStack', {
+new HelloWorldCdkLambdaStack(app, 'HelloWorldCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/hello_world_cdk-stack.ts
+++ lib/hello_world_cdk-stack.ts
@@ -1,16 +1,16 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
-export class HelloWorldCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class HelloWorldCdkLambdaStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloWorldCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+// The code that defines your stack goes here
+    const helloWorldFunction = new lambda.Function(this,"HelloWorldFunction", {
+      code: lambda.Code.fromAsset("functions/helloworld"),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_12_X,
+      description: "This Lambda Function returns the message 'Heloo, World!' in the reponse body",
+    });
   }
 }
